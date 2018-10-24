6598b6fd1c891a1cb293b9349a654651b4c78798
Cozart98 <chihaoui1098@gmail.com>
2018-10-24 17:46:50 +0200

    init project

---
+++ app/src/main/java/com/example/cozart/lend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cozart.lend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
